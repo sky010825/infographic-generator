e8e252cc1a27a579331b0c7c4a5cc4376c1027bf
sky010825 <92528649+sky010825@users.noreply.github.com>
2024-05-06 21:15:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,45 +5,58 @@ import io
 
 def main():
     # Streamlit Project 제목
-    st.title('Infograpic Generator')
+    st.title('Infograpic Generator 	:frame_with_picture:')
     st.text("<www.pexels.com> Background Photos provided by pexels")
-    num_images = st.number_input("Enter the number of images", min_value=1, max_value=5, value=3)
     st.divider() 
     
     if 'button_clicked' not in st.session_state:
         st.session_state.button_clicked = {}
     
+    if 'slides' not in st.session_state:
+        st.session_state.slides = []
     
-    for i in range(1, num_images+1):
-        col1, col2 = st.columns([3,7])
-        with col1:  
-            query = st.text_input(f'background keyword {i}')
-            txt = st.text_input(f'text keyword {i}')
-            
-        with col2:
-            if st.button(f"Make {i}", key=f"make_button_{i}"):
-                st.session_state.button_clicked[f"make_button_{i}"]=True
-                make(query, txt) 
+
+    # 추가
+    with st.form(key="form"):
+        query = st.text_input('배경 키워드')
+        txt = st.text_input('text text text')
+        submit = st.form_submit_button(':heavy_plus_sign:')
+        if submit:
+            if not query:
+                st.error("배경 키워드를 입력하세요.")
             else:
-                st.session_state.button_clicked[f"make_button_{i}"]=False
-        st.divider()
-      
-
-
-def make(query, txt):
-    num_images = 1
-    api_key = "  " ##Input your API KEY 
-    images = apply_box_to_images(api_key, query, txt, per_page=num_images)
-        
-    for i, image in enumerate(images):
-        st.image(image, width=400)   #이미지 띄우기
+                st.session_state.slides.append({'query':query,'txt':txt})
     
-        # 다운로드 버튼
-        image_file = io.BytesIO()
-        image.save(image_file, format='JPEG')
-        image_data = image_file.getvalue()  
-        download_button_label = f"Download Image"
-        st.download_button(label=download_button_label, data=image_data, file_name=f"image_{i+1}.jpg")
+    # 슬라이드 
+    for i in range(len(st.session_state.slides)):
+        col1, col2 = st.columns([0.8, 0.2])
+
+        with col1:
+            image = make(st.session_state.slides[i]['query'], st.session_state.slides[i]['txt'])
+            st.image(image)
+
+        with col2:
+            #삭제버튼
+            delete_button = st.button(label="삭제", key=i)
+            if delete_button:
+                del st.session_state.slides[i]
+                st.rerun()
+            
+            #다운로드 버튼
+            download(image, i)  
+
+
+
+
+
+
+
+def download(image, n):
+    for i, image in enumerate(image):
+        image_byte_array = io.BytesIO()
+        image.save(image_byte_array, format='JPEG')
+        image_data = image_byte_array.getvalue() 
+        st.download_button(label=f"다운로드 {i+n+1}", data=image_data, file_name=f"image_{i+n+1}.jpg")
 
 
 if __name__ == "__main__":
