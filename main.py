60448eee52dd12904120f7d1b0773d1e99943b35
sky010825 <92528649+sky010825@users.noreply.github.com>
2024-05-14 14:11:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,68 @@
-import streamlit as st 
+import streamlit as st
 from image import *
 from text import *
 import io
+import time
 
 def main():
-    # Streamlit Project 제목
-    st.title('Infograpic Generator 	:frame_with_picture:')
+    st.title('Infographic Generator :frame_with_picture:')
     st.text("<www.pexels.com> Background Photos provided by pexels")
-    st.divider() 
-    
-    if 'button_clicked' not in st.session_state:
-        st.session_state.button_clicked = {}
-    
+    st.divider()
+
     if 'slides' not in st.session_state:
         st.session_state.slides = []
     
+    if 'button_clicked' not in st.session_state:
+            st.session_state.button_clicked = {}
 
-    # 추가
-    with st.form(key="form"):
-        query = st.text_input('배경 키워드')
-        txt = st.text_input('text text text')
-        submit = st.form_submit_button(':heavy_plus_sign:')
-        if submit:
-            if not query:
-                st.error("배경 키워드를 입력하세요.")
-            else:
-                st.session_state.slides.append({'query':query,'txt':txt})
-    
-    # 슬라이드 
+    #input form
+    inputform()
+
+    #슬라이드 결과 보여주기
     for i in range(len(st.session_state.slides)):
-        col1, col2 = st.columns([0.8, 0.2])
+        image = overlay_image(st.session_state.slides[i]['query'], st.session_state.slides[i]['text'])
+        st.image(image)
 
-        with col1:
-            image = make(st.session_state.slides[i]['query'], st.session_state.slides[i]['txt'])
-            st.image(image)
 
-        with col2:
-            #삭제버튼
-            delete_button = st.button(label="삭제", key=i)
-            if delete_button:
-                del st.session_state.slides[i]
-                st.rerun()
+
+
+def inputform():
+    add_input = st.button("추가")
+    if 'num_inputs' not in st.session_state:
+            st.session_state.num_inputs = 0
+
+    if add_input:
+        st.session_state.num_inputs += 1
+
+    with st.form("form"):
+        inputs = []
+        for i in range(st.session_state.num_inputs):
+            query = st.text_input(f"슬라이드 {i+1} 이미지", key=f"image_keyword_{i}")
+            inputs.append(query)
+
+            text = st.text_input(f"슬라이드 {i+1} 문장", key=f"text_{i}")
+            inputs.append(text)
+
+        submit = st.form_submit_button("생성")
+        if submit:
+            for i in range(st.session_state.num_inputs):
+                query_key = f"image_keyword_{i}"
+                text_key = f"text_{i}"
+                query = st.session_state[query_key]
+                text = st.session_state[text_key]
+                st.session_state.slides.append({'query': query, 'text': text})
+
+            with st.spinner('Wait for it...'):
+                time.sleep(5)
             
-            #다운로드 버튼
-            download(image, i)  
-
-
-
-
-
 
 
 def download(image, n):
     for i, image in enumerate(image):
         image_byte_array = io.BytesIO()
         image.save(image_byte_array, format='JPEG')
-        image_data = image_byte_array.getvalue() 
+        image_data = image_byte_array.getvalue()
         st.download_button(label=f"다운로드 {i+n+1}", data=image_data, file_name=f"image_{i+n+1}.jpg")
-
 
 if __name__ == "__main__":
     main()
-
-    
